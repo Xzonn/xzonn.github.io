1c11a21fd72896424548dd50c1abfa22701ce81e
Xzonn <xzonn2000@gmail.com>
2019-03-17 20:54:07 +0800

    2019-03-17

--- main.js
+++ main.js
@@ -97,10 +97,12 @@ $(function() {
     })();
 
     // 代码高亮
-    hljs.initHighlighting();
-    $("#content pre code").each(function() {
-        $(this).html("<ul><li>" + $(this).html().replace(/^\s+/, "").replace(/\s+$/, "").replace(/\n/g, "</li><li>") + "</li></ul>");
-    });
+    if (window.hljs) {
+        hljs.initHighlighting();
+        $("#content pre code").each(function() {
+            $(this).html("<ul><li>" + $(this).html().replace(/^\s+/, "").replace(/\s+$/, "").replace(/\n/g, "</li><li>") + "</li></ul>");
+        });
+    }
 
     //img标签相关
     var imageDisplay = ["auto", "none", "block", "left", "right", "center"],
