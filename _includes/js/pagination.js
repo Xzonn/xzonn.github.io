290b247a1d02448824a4859a943945b183c319e7
Xzonn <Xzonn2000@gmail.com>
2020-07-30 17:53:16 +0800

    2020-07-30/2

--- _includes/js/pagination.js
+++ _includes/js/pagination.js
@@ -3,15 +3,15 @@ let renderPagination = function (pageNumber, maxPageNumber, clickEvent) {
         addPage = function (page, text, addClass) {
             $("<a/>").text(text || page).attr("href", "#").data("page", addClass ? NaN : page).appendTo($("<li/>").addClass(addClass).appendTo(paginationList));
         };
-    addPage(pageNumber - 1, "«", pageNumber == 1 ? "disabled" : "");
+    addPage(pageNumber - 1, "\xab", pageNumber == 1 ? "disabled" : "");
     (pageNumber > 3) && addPage(1);
-    (pageNumber > 4) && addPage(1, "…", "disabled");
+    (pageNumber > 4) && addPage(1, "\u2026", "disabled");
     for (let i = Math.max(1, pageNumber - 2); i <= Math.min(maxPageNumber, pageNumber + 2); i++) {
         addPage(i, i, pageNumber == i ? "active" : "");
     }
-    (pageNumber < maxPageNumber - 3) && addPage(1, "…", "disabled");
+    (pageNumber < maxPageNumber - 3) && addPage(1, "\u2026", "disabled");
     (pageNumber < maxPageNumber - 2) && addPage(maxPageNumber);
-    addPage(pageNumber + 1, "»", pageNumber == maxPageNumber ? "disabled" : "");
+    addPage(pageNumber + 1, "\xbb", pageNumber == maxPageNumber ? "disabled" : "");
     paginationList.find("a").bind("click", clickEvent);
     return(paginationList);
 }
--- js/studySearch.js
+++ js/studySearch.js
@@ -31,15 +31,15 @@ let renderPagination = function (pageNumber, maxPageNumber, clickEvent) {
         addPage = function (page, text, addClass) {
             $("<a/>").text(text || page).attr("href", "#").data("page", addClass ? NaN : page).appendTo($("<li/>").addClass(addClass).appendTo(paginationList));
         };
-    addPage(pageNumber - 1, "Â«", pageNumber == 1 ? "disabled" : "");
+    addPage(pageNumber - 1, "\xab", pageNumber == 1 ? "disabled" : "");
     (pageNumber > 3) && addPage(1);
-    (pageNumber > 4) && addPage(1, "â€¦", "disabled");
+    (pageNumber > 4) && addPage(1, "\u2026", "disabled");
     for (let i = Math.max(1, pageNumber - 2); i <= Math.min(maxPageNumber, pageNumber + 2); i++) {
         addPage(i, i, pageNumber == i ? "active" : "");
     }
-    (pageNumber < maxPageNumber - 3) && addPage(1, "â€¦", "disabled");
+    (pageNumber < maxPageNumber - 3) && addPage(1, "\u2026", "disabled");
     (pageNumber < maxPageNumber - 2) && addPage(maxPageNumber);
-    addPage(pageNumber + 1, "Â»", pageNumber == maxPageNumber ? "disabled" : "");
+    addPage(pageNumber + 1, "\xbb", pageNumber == maxPageNumber ? "disabled" : "");
     paginationList.find("a").bind("click", clickEvent);
     return(paginationList);
 }
